639f2620a6179a1dd411357e70d696bcd1e3b754
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2023-10-29 00:18:10 +0530

    Add files via upload

---
+++ ASSINGMENT.cpp/4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius,height;
+    float pi=3.14;
+    cout<<"enter radius=";
+    cin>>radius;
+    cout<<"enter height=";
+    cin>>height;
+    cout<<"volume of cylinder="<<2*pi*radius*height<<endl;
+    return 0;
+
+}
---
+++ ASSINGMENT.cpp/5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    char x,y;
+    cout<<"enter first character=";
+    cin>>x;
+    cout<<"enter second character=";
+    cin>>y;
+    cout<<"ASCII VALUE OF FIRST CHARACTER="<<(int)x<<endl;
+    cout<<"ASCII VALUE OF SECOND CHARACTER="<<(int)y<<endl;
+    return 0;
+
+}
